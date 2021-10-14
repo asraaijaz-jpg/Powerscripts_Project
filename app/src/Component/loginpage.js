78454b762c8e01c53d719a069c9cea5afdc6e631
asra <asraaijaz.se17@iba-suk.edu.pk>
2021-10-14 13:36:19 +0500

    commit

--- app/src/Component/loginpage.js
+++ app/src/Component/loginpage.js
@@ -29,7 +29,7 @@ function LoginPage()
 {
     useEffect(()=>{
         document.title='Login Page';
-        // alert(process.env.PUBLIC_URL);
+         console.log(process.env.PUBLIC_URL);
     });
     const [isPreview, setIsPreview] = useState(false);
     // const isPreview=false;
